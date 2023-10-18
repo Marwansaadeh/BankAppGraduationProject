c6301f5a2a81c2202acccc5fa4ebb1613dc6da45
Marwan <marwansaad123@hotmail.com>
2023-10-18 15:40:40 +0200

    Add project files.

---
+++ BankAppWebService/BankAppService.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankAppWebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BankAppService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select BankAppService.svc or BankAppService.svc.cs at the Solution Explorer and start debugging.
+    public class BankAppService : IBankAppService
+    {
+        [AuthorizationFilter]
+        string IBankAppService.GetCustomer()
+        {
+            return "Hello from Webapp";
+        }
+    }
+    public class AuthorizationFilter : AuthorizeAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (filterContext.ActionDescriptor.IsDefined(typeof(AllowAnonymousAttribute), true)
+                || filterContext.ActionDescriptor.ControllerDescriptor.IsDefined(typeof(AllowAnonymousAttribute), true))
+            {
+                // Don't check for authorization as AllowAnonymous filter is applied to the action or controller
+                return;
+            }
+
+            // Check for authorization
+            if (HttpContext.Current.Session["UserName"] == null)
+            {
+                filterContext.Result = new HttpUnauthorizedResult();
+            }
+
+        }
+    }
+}
---
+++ MobilApp/Controllers/ArrayInputAttribute.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace MobilApp.Controllers
+{
+
+    public class ArrayInputAttribute : ActionFilterAttribute
+    {
+        private readonly string _parameterName;
+
+        public ArrayInputAttribute(string parameterName)
+        {
+            _parameterName = parameterName;
+            Separator = ',';
+        }
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (actionContext.ActionArguments.ContainsKey(_parameterName))
+            {
+                string parameters = string.Empty;
+                if (actionContext.ControllerContext.RouteData.Values.ContainsKey(_parameterName))
+                    parameters = (string)actionContext.ControllerContext.RouteData.Values[_parameterName];
+                else if (actionContext.ControllerContext.Request.RequestUri.ParseQueryString()[_parameterName] != null)
+                    parameters = actionContext.ControllerContext.Request.RequestUri.ParseQueryString()[_parameterName];
+
+                actionContext.ActionArguments[_parameterName] = parameters.Split(Separator).Select(int.Parse).ToArray();
+            }
+        }
+
+        public char Separator { get; set; }
+    }
+}
